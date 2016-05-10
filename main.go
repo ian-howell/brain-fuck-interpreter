c29d1b18ee30addabec63880fed5ab86f49f9153
Ian Howell <ian.howell0@gmail.com>
2016-05-09 21:38:48 -0500

    Read in the program instructions

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
 	"strings"
 )
 
-func main(argc int, argv []*byte) {
-	r := strings.NewReader(argv[1])
+func main() {
+	// Set usage message
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s filename\n", os.Args[0])
+	}
+
+	// Parse the command line arguments
+	flag.Parse()
+
+	argc := flag.NArg()
+
+	if argc != 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	program := flag.Args()[0]
+
+	if !strings.HasSuffix(program, ".bf") {
+		log.Fatalf("%s does not to be a brainfuck program\n", program)
+	}
+
+	instructions, err := ioutil.ReadFile(program)
+	if err != nil {
+		log.Fatalf("There was a problem when opening %s\n", program)
+	}
 }
